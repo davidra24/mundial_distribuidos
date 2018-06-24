7ecc8afbbfbf9b9216bf218eb31564c14403e7f6
David Ramirez <cdavidramirezg24@gmail.com>
2018-06-24 01:08:59 -0500

    Double param question search

--- routes/index.js
+++ routes/index.js
@@ -32,14 +32,14 @@ router.delete('/api/trivias/:id', query.removeTrivia);
 /* ---------------- Question Trivias Api ----------------- */
 router.get('/api/questionTrivias', query.getAllQuestionTrivias);
 router.get('/api/questionTrivias/:id_trivia/:id_question', query.getSingleQuestionTrivia);
-router.post('/api/questionTrivias', query.createQuestionTrivia);
+router.post('/api/questionTrivias:id_trivia/:id_question', query.createQuestionTrivia);
 router.delete('/api/questionTrivias/:/:id_trivia/:id_question', query.removeQuestionTrivia);
 
 /* ---------------- User Trivias Api ----------------- */
 router.get('/api/userTrivias', query.getAllUserTrivias);
 router.get('/api/userTrivias/:id_trivia/:id_user', query.getSingleUserTrivia);
 router.post('/api/userTrivias', query.createUserTrivia);
-router.put('/api/userTrivias/:/:id_trivia/:id_user', query.updateUserTrivia);
+router.put('/api/userTrivias/:id_trivia/:id_user', query.updateUserTrivia);
 router.delete('/api/userTrivias/:/:id_trivia/:id_question', query.removeUserTrivia);
 
 module.exports = router;
