5755610daf257b4273ea9a893bbc3f29c2a0b059
David Ramirez <cdavidramirezg24@gmail.com>
2018-06-24 16:35:03 -0500

    Preguntas de  cada trivia

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ router.delete('/api/trivias/:id', query.removeTrivia);
 /* ---------------- Question Trivias Api ----------------- */
 router.get('/api/questionTrivias', query.getAllQuestionTrivias);
 router.get('/api/questionTrivias/:id_trivia/:id_question', query.getSingleQuestionTrivia);
-router.get('/api/questionTrivias/:id_trivia', query.getAllQuestionsTrivia);
+router.get('/api/questionTrivias/:id_trivia', query.getAllQuestionsInTrivia);
 router.post('/api/questionTrivias/', query.createQuestionTrivia);
 router.delete('/api/questionTrivias/:id_trivia/:id_question', query.removeQuestionTrivia);
 
